12134d783912be80a146f2783afb88c9cd2066d9
Emelyn Barrientos <111668417+emelyn-barrientos@users.noreply.github.com>
2022-11-27 18:42:35 +1300

    Import and map out books array for each list item

--- client/components/AllBooksList.jsx
+++ client/components/AllBooksList.jsx
@@ -1,30 +1,29 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
+import Book from './Book'
+
 import { fetchBooks } from '../actions'
 
 function AllBooksList() {
+
   const books = useSelector((state) => state.allBooks)
+
   const dispatch = useDispatch()
+
   useEffect(() => {
     dispatch(fetchBooks())
-  }, [])
+  },[])
 
   return (
     <div>
-      <h3>Recent hot takes on popular reads:</h3>
       <ul>
-        {books && books.map((book) => {
-          return <li key={book.id}>
-            <p>Title:  {book.title}</p>
-            <p>Author:  {book.author}</p>
-            <p>My Rating:  {book.rating}</p>
-          </li>
-        }
-        )}
+        {books.map(book => {
+          return <Book key={book.id} title={book.title} author={book.author} rating={book.rating}/>
+        })}
       </ul>
     </div>
-)
+  )
 }
 
 export default AllBooksList
