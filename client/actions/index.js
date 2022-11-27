211f254ed5398cc4a5bcab7f0241dc44199d67a4
Emelyn Barrientos <111668417+emelyn-barrientos@users.noreply.github.com>
2022-11-28 12:37:22 +1300

    Add ADD_BOOK action and function

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,14 +1,25 @@
+export const SET_BOOKS = 'SET_BOOKS'
+export const ADD_BOOK ='ADD_BOOK'
+
+
 import { getListOfBooks } from '../apis/booksApi'
 
-export const SET_BOOKS = 'SET_BOOKS'
-
 export function setBooks(books) {
+  console.log('setBooks payload: ', books)
   return {
     type: SET_BOOKS,
     payload: books,
   }
 }
 
+export function addBook(newBook) {
+  return {
+    type: ADD_BOOK,
+    payload: newBook
+  }
+}
+
+// THUNKS 
 export function fetchBooks() {
   return (dispatch) => {
     return getListOfBooks().then((books) => {
