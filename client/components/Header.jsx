5b389a07f4912a94f067f81a53e009541ed0f30d
Emelyn Barrientos <111668417+emelyn-barrientos@users.noreply.github.com>
2022-11-27 15:41:34 +1300

    Create initial Header component

---
+++ client/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+
+  return (
+    <>
+      <div>
+        <h1>Better Reads</h1>
+        <h2>Because books deserve better & I have some hot takes on the most popular titles.</h2>
+      </div>
+    </>
+  )
+}
+
+export default Header
