049e7082721878a90b05b2e512ce1958b4d4d354
Emelyn Barrientos <111668417+emelyn-barrientos@users.noreply.github.com>
2022-11-29 11:32:50 +1300

    Add useDispatch and onClick handler for delete id

--- client/components/Book.jsx
+++ client/components/Book.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import { useDispatch } from 'react-redux'
+
+import { removeBook } from '../actions'
+
+// DELETE BOOK FEATURE
+// [x] Import useDispatch
+// [x] Add onClick event handler and button
+// [x] Dispatch book ID to thunk
+// [] Make deleteBook thunk
+// [] Make deleteBook api request
+// [] Import removeBook from actions
+// [] Make deleteBook action
+
+
 function Book(props) {
 
   const { id, title, author, rating } = props
+
+  const dispatch = useDispatch()
+
+  const handleClick = (id) => {
+    dispatch(removeBook(id))
+  }
 
   return (
     <div>
@@ -10,6 +30,7 @@ function Book(props) {
         <p>Title:  {title}</p>
         <p>Author:  {author}</p>
         <p>badreads Rating:  {rating}</p>
+        <button className='delete' onClick={handleClick}>Delete</button>
       </li>
     </div>
   )
