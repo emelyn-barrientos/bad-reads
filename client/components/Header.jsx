8bb742f4ce119b1ea72d24790c072fbac6f73936
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2022-11-29 22:57:06 +1300

    Add flex stylying to body and header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
+    <div className="header">
       <h1>badreads</h1>
       <h2>
         Because readers deserve better to know the real tea with overhyped
